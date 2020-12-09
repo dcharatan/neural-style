4b487fd169d2a8f6a7d5ce01355449006262042b
David Charatan <david.charatan@gmail.com>
2020-12-09 13:38:59 -0800

    Clean up demo script a bit.

--- source/image.py
+++ source/image.py
@@ -38,12 +38,13 @@ def get_pillow_transform(image_size: Optional[int]):
     return tf.Compose(transforms)
 
 
-def load_image(file_name: str, image=None) -> torch.Tensor:
+def load_image(file_name: Optional[str], image=None) -> torch.Tensor:
     """Load an image so that its shape is (B, C, H, W) and it's normalized to
     the range [0, 1].
     """
     transform = get_pillow_transform(None)
-    if file_name != "":
+    if file_name is not None:
+        assert image is None
         image = Image.open(file_name)
     return transform(image).unsqueeze(0)
 
--- source/script_modules/run_pretrained_model.py
+++ source/script_modules/run_pretrained_model.py
@@ -9,79 +9,98 @@ import argparse
 from moviepy.editor import VideoFileClip, ImageSequenceClip
 import os
 import shutil
+import time
+
+MODEL_PATH = "saved_models/mosaic_30k.pth"
+INPUT_PATH = "test/stadt.jpg"
+OUTPUT_PATH = "result/test1.jpg"
+VIDEO_IN = "test/"
+VIDEO_OUT = "result/"
+TMP_DIR = "tmp"
+IMAGE_WIDTH = 800
+IMAGE_HEIGHT = 600
+
+resize = tf.Compose(
+    [
+        tf.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+        tf.CenterCrop((IMAGE_HEIGHT, IMAGE_WIDTH)),
+    ]
+)
 
 if __name__ == "__main__":
     # Parse arguments
     arg_parser = argparse.ArgumentParser(description="Neural Style Transfer")
-    arg_parser.add_argument("-v", "--video", default=False, action="store_true",
-                            help="toggle for style transfer on video stream")
-    arg_parser.add_argument("-w", "--webcam", default=False, action="store_true",
-                            help="toggle for style transfer on webcam stream")
+    arg_parser.add_argument(
+        "-v",
+        "--video",
+        default=False,
+        action="store_true",
+        help="toggle for style transfer on video stream",
+    )
+    arg_parser.add_argument(
+        "-w",
+        "--webcam",
+        default=False,
+        action="store_true",
+        help="toggle for style transfer on webcam stream",
+    )
     args = vars(arg_parser.parse_args())
-
-    MODEL_PATH = "saved_models/final_model.pth"
-    INPUT_PATH = "test/stadt.jpg"
-    OUTPUT_PATH = "result/test1.jpg"
-    VIDEO_IN = "test/"
-    VIDEO_OUT = "result/"
-    TMP_DIR = "tmp"
-    IMAGE_SIZE = 256
-    BATCH_SIZE = 4
 
     # Load the pre-trained model.
     model = StylizationModel()
-    # model.load_state_dict(torch.load(MODEL_PATH))
+    model.load_state_dict(torch.load(MODEL_PATH))
     model.eval()
 
     if args["video"]:
         video_clip = VideoFileClip(VIDEO_IN, audio=False)
         current_directory = os.getcwd()
-        tmp_directory = os.path.join(current_directory,TMP_DIR)
-        os.makedirs(tmp_directory) 
+        tmp_directory = os.path.join(current_directory, TMP_DIR)
+        os.makedirs(tmp_directory)
 
         for i, frame in enumerate(video_clip.iter_frames()):
             frame = get_numpy_transform(frame)
             pred = model(frame).detach().numpy()[0]
-            save_image(tmp_directory + '/' + str(i).zfill(5) + '.png')
+            save_image(tmp_directory + "/" + str(i).zfill(5) + ".png")
 
-        ImageSequenceClip(sequence=tmp_directory + '/', fps=video_clip.fps).write_videofile(VIDEO_OUT)
-        shutil.rmtree(tmp_directory + '/')
-        
+        ImageSequenceClip(
+            sequence=tmp_directory + "/", fps=video_clip.fps
+        ).write_videofile(VIDEO_OUT)
+        shutil.rmtree(tmp_directory + "/")
 
-
-    if args["webcam"]: # webcam mode, process frames in webcam stream
-        cap = cv2.VideoCapture(0)
-        while(True):
+    if args["webcam"]:
+        # webcam mode, process frames in webcam stream
+        cv2.startWindowThread()
+        cv2.namedWindow("frame")
+        while True:
+            # Read an image.
+            cap = cv2.VideoCapture(0)
             ret, frame = cap.read()
-            test = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            test = load_image("", Image.fromarray(test))
-            test = tf.Compose(
-                [
-                    tf.Resize(IMAGE_SIZE),
-                    tf.CenterCrop(IMAGE_SIZE),
-                ]
-            )(test)
-            output_test = model(test).detach().numpy()
+            cap.release()
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = load_image(None, Image.fromarray(image))
+            image = resize(image)
+
+            # Evaluate the model.
+            t = time.time()
+            output_test = model(image).detach().numpy()
+            print(f"Evaluating model took {time.time() - t} seconds.")
+
+            # Send the result to the display.
             output_test = np.transpose(output_test[0, :, :, :], (1, 2, 0))
             output_test = np.clip(output_test, 0, 1)
             output_test = np.uint8(output_test * 255)
             output_test = cv2.cvtColor(output_test, cv2.COLOR_RGB2BGR)
-            output_test = cv2.flip(output_test, 1) # mirrors webcam frame
-            cv2.imshow('frame', output_test)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            output_test = cv2.flip(output_test, 1)  # mirrors webcam frame
+            cv2.imshow("frame", output_test)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
-        cap.release()
         cv2.destroyAllWindows()
 
-    else: # normal mode, just process image
+    else:
+        # Normal mode: just process an image.
         # Run the image through the model.
-        test = load_image(INPUT_PATH)
-        test = tf.Compose(
-            [
-                tf.Resize(IMAGE_SIZE),
-                tf.CenterCrop(IMAGE_SIZE),
-            ]
-        )(test)
-        output_test = model(test).detach().numpy()
+        image = load_image(INPUT_PATH)
+        image = resize(image)
+        output_test = model(image).detach().numpy()
         save_image(OUTPUT_PATH, output_test[0, :, :, :])
