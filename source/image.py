2f9e2bcc702a11a0b2fff429044296dc5506f6e4
David Charatan <david.charatan@gmail.com>
2020-12-09 11:13:23 -0800

    Clean up per channel normalize.

--- source/image.py
+++ source/image.py
@@ -7,17 +7,12 @@ from typing import Optional
 NORMALIZE = tf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
-def custom_normalize(x: torch.Tensor):
-    if x.ndim == 4:
-        mu = torch.mean(x, dim=[2, 3])
-        std = torch.std(x, dim=[2, 3])
-        return 0.5 + (x - mu[:, None, None, None]) / (2 * std[:, None, None, None])
-    elif x.ndim == 3:
-        mu = torch.mean(x, dim=[1, 2])
-        std = torch.std(x, dim=[1, 2])
-        return 0.5 + (x - mu[:, None, None]) / (2 * std[:, None, None])
-    else:
+def per_channel_normalize(x: torch.Tensor):
+    if x.ndim != 3:
         raise Exception("not implemented")
+    mu = torch.mean(x, dim=[1, 2])
+    std = torch.std(x, dim=[1, 2])
+    return 0.5 + (x - mu[:, None, None]) / (2 * std[:, None, None])
 
 
 def get_data_loader_transform(image_size: int):
@@ -25,7 +20,7 @@ def get_data_loader_transform(image_size: int):
         tf.Resize(image_size),
         tf.CenterCrop(image_size),
         tf.ToTensor(),
-        custom_normalize,
+        per_channel_normalize,
     ]
     return tf.Compose(transforms)
 
@@ -39,7 +34,7 @@ def get_pillow_transform(image_size: Optional[int]):
             tf.CenterCrop(image_size),
         ]
     transforms.append(tf.ToTensor())
-    transforms.append(custom_normalize)
+    transforms.append(per_channel_normalize)
     return tf.Compose(transforms)
 
 
@@ -52,9 +47,11 @@ def load_image(file_name: str, image=None) -> torch.Tensor:
         image = Image.open(file_name)
     return transform(image).unsqueeze(0)
 
+
 def get_numpy_transform(image) -> torch.Tensor:
     transform = get_pillow_transform(None)
     return transform(image)
+
 
 def save_image(file_name: str, image: np.ndarray):
     # Go from (C, H, W) to (H, W, C).
