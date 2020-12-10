286bf1dd75df6d5c2c82eafd0a0c68c9cb79b1bb
David Charatan <david.charatan@gmail.com>
2020-12-09 16:25:08 -0800

    Make training load from settings file.

---
+++ source/SettingsLoader.py
@@ -0,0 +1,44 @@
+import json
+from argparse import ArgumentParser
+from typing import Optional
+
+
+class SettingsLoader:
+    SETTINGS_DEFAULTS = {
+        "image_size": 256,
+        "batch_size": 4,
+        "learning_rate": 1e-3,
+        "num_epochs": 1,
+        "feature_weight": 1,
+        "style_weight": 20000,
+        "results_folder": "results/mosaic",
+        "style_image": "style/mosaic.jpg",
+        "save_model_every": 1000,
+        "save_image_every": 25,
+        "print_status_every": 50,
+    }
+
+    @staticmethod
+    def load_settings_from_argv():
+        parser = ArgumentParser(description="Script with JSON Settings File")
+        parser.add_argument("--file_name", help="The JSON settings file.")
+        args = parser.parse_args()
+        return SettingsLoader.load_settings(args.file_name)
+
+    @staticmethod
+    def load_settings(file_name: Optional[str]):
+        if file_name is None:
+            return SettingsLoader.SETTINGS_DEFAULTS
+
+        with open(file_name, "r") as f:
+            settings = json.load(f)
+        SettingsLoader.validate_settings(settings)
+
+        # Use the defaults for unspecified settings.
+        return {**SettingsLoader.SETTINGS_DEFAULTS, **settings}
+
+    @staticmethod
+    def validate_settings(settings: dict):
+        for key in settings:
+            if key not in SettingsLoader.SETTINGS_DEFAULTS:
+                raise Exception(f'Unknown setting "{key}"')
--- source/script_modules/train_model.py
+++ source/script_modules/train_model.py
@@ -1,25 +1,30 @@
-from source.image import get_data_loader_transform, load_image, save_image
+import os
 import torch
+import torchvision
+import time
+from pathlib import Path
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-import torchvision
-import numpy as np
-import time
+from ..image import get_data_loader_transform, load_image, save_image
 from ..models.StylizationModel import StylizationModel
 from ..models.FeatureLossModel import FeatureLossModel
 from .. import util
-import os
+from ..SettingsLoader import SettingsLoader
+
+# Load the settings and make folders for the results.
+settings = SettingsLoader.load_settings_from_argv()
 
 
-# TODO: Constants -- tune later
-IMAGE_SIZE = 256
-BATCH_SIZE = 4
-LEARNING_RATE = 1e-3
-EPOCHS = 1
-FEATURE_WEIGHT = 1
-STYLE_WEIGHT = 20000
-MODEL_FOLDER = "saved_models"
-STYLE_IMAGE = "style/mosaic.jpg"
+def with_folder(file_name: str):
+    return os.path.join(settings["results_folder"], file_name)
+
+
+def make_folder(folder_name: str):
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+
+make_folder(with_folder("intermediate_images"))
+make_folder(with_folder("intermediate_models"))
 
 # Use CUDA if it's available.
 device = torch.device("cpu")
@@ -30,26 +35,27 @@ feature_model = FeatureLossModel([(1, 2), (2, 2), (3, 3), (4, 3)]).to(device)
 
 # Set up the data loader.
 trainset = torchvision.datasets.ImageFolder(
-    "content", get_data_loader_transform(IMAGE_SIZE)
+    "content", get_data_loader_transform(settings["image_size"])
 )
-train_loader = DataLoader(trainset, batch_size=BATCH_SIZE)
+batch_size = settings["batch_size"]
+train_loader = DataLoader(trainset, batch_size=batch_size)
 train_loader_len = len(train_loader)
 
 # Load the style image.
-style = load_image(STYLE_IMAGE).repeat(BATCH_SIZE, 1, 1, 1).to(device)
+style = load_image(settings["style_image"]).repeat(batch_size, 1, 1, 1).to(device)
 vgg_style = feature_model(style)
 gram_style = {name: util.gram_matrix(tensor) for name, tensor in vgg_style.items()}
 
 # This is equivalent to the squared normalized euclidean distance.
 norm = torch.nn.MSELoss()
-optimizer = Adam(style_model.parameters(), LEARNING_RATE)
+optimizer = Adam(style_model.parameters(), settings["learning_rate"])
 style_model.train()
-for e in range(EPOCHS):
+for e in range(settings["num_epochs"]):
     epoch_start_time = time.time()
-    for batch_index, (x, _) in enumerate(train_loader):
+    for batch_index, (images, _) in enumerate(train_loader):
         # The image x is normalized to [0, 1].
         batch_start_time = time.time()
-        x = x.to(device)
+        x = images.to(device)
         optimizer.zero_grad()
 
         # Get the stylized output.
@@ -64,7 +70,9 @@ for e in range(EPOCHS):
         vgg_x = feature_model(x)
 
         # Compute the feature reconstruction loss.
-        feature_loss = FEATURE_WEIGHT * norm(vgg_y_hat["relu2_2"], vgg_x["relu2_2"])
+        feature_loss = settings["feature_weight"] * norm(
+            vgg_y_hat["relu2_2"], vgg_x["relu2_2"]
+        )
 
         # Compute the style reconstruction loss.
         style_loss = 0.0
@@ -73,28 +81,45 @@ for e in range(EPOCHS):
         }
         for name in vgg_y_hat.keys():
             style_loss += norm(gram_y_hat[name], gram_style[name])
-        style_loss *= STYLE_WEIGHT
+        style_loss *= settings["style_weight"]
 
         total_loss = feature_loss + style_loss
         total_loss.backward()
         optimizer.step()
 
-        if batch_index % 50 == 0:
+        # Left-pad the epoch and batch indices for printing.
+        e_str = str(e).zfill(len(str(settings["num_epochs"])))
+        b_str = str(batch_index).zfill(len(str(len(train_loader))))
+
+        # Print a status message.
+        if batch_index % settings["print_status_every"] == 0:
+            print("===============================")
             print(
-                f"Batch {batch_index} of {train_loader_len} took {time.time() - batch_start_time} seconds."
+                f"Batch {b_str} of {train_loader_len} took {time.time() - batch_start_time} seconds."
             )
             print(f"Feature loss: {feature_loss.data.item()}")
             print(f"Style loss: {style_loss.data.item()}")
-        if batch_index % 25 == 0:
-            save_image(f"tmp_{e}_{batch_index}.png", y_hat[0].cpu().detach().numpy())
-        if batch_index % 1000 == 0:
-            torch.save(
-                style_model.state_dict(),
-                os.path.join(MODEL_FOLDER, f"tmp_model_{e}_{batch_index}.pth"),
+
+        # Save intermediate images.
+        if batch_index % settings["save_image_every"] == 0:
+            input = images[0]
+            output = y_hat[0].cpu().detach().numpy()
+            save_image(
+                with_folder(f"intermediate_images/image_{e_str}_{b_str}_input.png"),
+                input,
+            )
+            save_image(
+                with_folder(f"intermediate_images/image_{e_str}_{b_str}_output.png"),
+                output,
             )
 
-    print(f"Epoch: {e + 1} took {time.time() - epoch_start_time} seconds.")
+        # Save the model.
+        if batch_index % settings["save_model_every"] == 0 and batch_index > 0:
+            torch.save(
+                style_model.state_dict(),
+                with_folder(f"intermediate_models/model_{e_str}_{b_str}.pth"),
+            )
 
 # Save the trained style_model.
 style_model.eval()
-torch.save(style_model.state_dict(), os.path.join(MODEL_FOLDER, "final_model.pth"))
+torch.save(style_model.state_dict(), with_folder("final_model.pth"))
