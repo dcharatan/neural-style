41d05ef0a438ea4c292c8e28a7f5c790deb58315
David Charatan <david.charatan@gmail.com>
2020-12-09 17:59:42 -0800

    Update grid script.

--- source/script_modules/make_image_grid.py
+++ source/script_modules/make_image_grid.py
@@ -2,12 +2,16 @@ import os
 import cv2
 import numpy as np
 import re
+from ..SettingsLoader import SettingsLoader
 
 # This script makes an image grid of training images for the presentation.
+# Run this after having run the training script to produce an image grid with
+# the first n saved training outputs.
 
-IMAGE_FOLDER = "."
+# Load the settings and make folders for the results.
+settings = SettingsLoader.load_settings_from_argv()
 
-TARGET_WIDTH = 1920
+TARGET_WIDTH = 1920 + 120 * 4
 TARGET_HEIGHT = 1080
 TILE_SIZE = 120
 
@@ -18,33 +22,14 @@ num_rows = TARGET_HEIGHT // TILE_SIZE
 num_cols = TARGET_WIDTH // TILE_SIZE
 num_images = num_rows * num_cols
 
-# Find all PNG files.
-files = []
-for file in os.listdir(IMAGE_FOLDER):
-    if file.endswith(".png"):
-        files.append(file)
+# Get the first num_images PNG files.
+folder = os.path.join(settings["results_folder"], "intermediate_images")
+files = os.listdir(folder)
+files = sorted(files)[:num_images]
 
-# Extract the valid files and sort them by index.
-valid_files = [f for f in files if re.match("tmp_[0-9]+_[0-9]+\.png", f)]
+assert len(files) == num_images and "If this fails you don't have enough images."
 
-
-def get_numbers(x):
-    first = x.index("_")
-    x = x[first + 1 :]
-    second = x.index("_")
-    num1 = int(x[:second])
-    x = x[second + 1 :]
-    third = x.index(".")
-    num2 = int(x[:third])
-    return num1, num2
-
-
-valid_files = sorted(valid_files, key=get_numbers)
-valid_files = valid_files[:num_images]
-
-assert len(valid_files) == num_images and "If this fails you don't have enough images."
-
-images = [cv2.imread(os.path.join(IMAGE_FOLDER, f)) for f in valid_files]
+images = [cv2.imread(os.path.join(folder, f)) for f in files]
 images = [cv2.resize(image, (TILE_SIZE, TILE_SIZE)) for image in images]
 
 grid = np.empty((TARGET_HEIGHT, TARGET_WIDTH, 3))
@@ -57,4 +42,4 @@ for row in range(num_rows):
             row_start : row_start + TILE_SIZE, col_start : col_start + TILE_SIZE, :
         ] = images[row * num_cols + col]
 
-cv2.imwrite("tmp_image_grid.png", grid)
+cv2.imwrite(os.path.join(settings["results_folder"], "image_grid.png"), grid)
